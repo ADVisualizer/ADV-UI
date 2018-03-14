5dbcc6109128392ad0a275871fe189a76ae7857f
Murièle Trentini <muriele@trentini.ch>
2018-03-14 08:42:12 +0100

    handle logback dependency and add config file

--- module-array/src/main/java/ch/adv/ui/array/ArrayModule.java
+++ module-array/src/main/java/ch/adv/ui/array/ArrayModule.java
@@ -1,11 +1,15 @@
 package ch.adv.ui.array;
 
 import ch.adv.ui.ADVModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ArrayModule implements ADVModule {
+    private static final Logger logger = LoggerFactory.getLogger(ArrayModule.class);
 
     @Override
     public void print() {
+        logger.info("printing...");
         System.out.println("Holla Array");
     }
 }
