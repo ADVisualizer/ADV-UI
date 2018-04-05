613efcf8308adabf1ac1d0629161fc050f10188b
Murièle Trentini <muriele@trentini.ch>
2018-04-05 11:18:43 +0200

    update flowControlTest

--- ui-core/src/test/java/ch/adv/ui/core/service/ADVFlowControlTest.java
+++ ui-core/src/test/java/ch/adv/ui/core/service/ADVFlowControlTest.java
@@ -1,34 +1,40 @@
 package ch.adv.ui.core.service;
 
+import ch.adv.ui.core.access.FileDatastoreAccess;
 import ch.adv.ui.core.access.Parser;
 import ch.adv.ui.core.app.ADVModule;
 import ch.adv.ui.core.domain.Session;
-import ch.adv.ui.core.domain.Snapshot;
 import ch.adv.ui.core.logic.ModuleStore;
 import ch.adv.ui.core.logic.SessionStore;
-import ch.adv.ui.core.presentation.Layouter;
+import ch.adv.ui.core.logic.SessionStoreTest;
 import ch.adv.ui.core.presentation.LayoutedSnapshotStore;
+import ch.adv.ui.core.presentation.Layouter;
 import ch.adv.ui.core.presentation.domain.LayoutedSnapshot;
+import com.google.gson.Gson;
 import com.google.inject.Inject;
 import javafx.scene.layout.Pane;
-import org.jukito.JukitoModule;
 import org.jukito.JukitoRunner;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 
 @RunWith(JukitoRunner.class)
 public class ADVFlowControlTest {
-
+    @Inject
+    private FileDatastoreAccess reader;
+    @Inject
+    private Gson gson;
     @Inject
     private ADVModule testModule;
     @Inject
@@ -37,8 +43,6 @@ public class ADVFlowControlTest {
     private Layouter testLayouter;
     @Inject
     private Session testSession;
-    @Inject
-    private Snapshot testSnapshot;
     @Inject
     private LayoutedSnapshot testLayoutedSnapshot;
     @Inject
@@ -52,44 +56,50 @@ public class ADVFlowControlTest {
     @Inject
     private ADVFlowControl flowControl;
 
+    private String testJSON;
+
     @Before
     public void setUp() {
-        List<Snapshot> snapshots = new ArrayList<>();
-        snapshots.add(testSnapshot);
-        Mockito.when(testSession.getSnapshots()).thenReturn(snapshots);
+        URL url1 = SessionStoreTest.class.getClassLoader()
+                .getResource("session1.json");
 
+        testJSON = reader.read(new File(url1.getPath()));
+
+        testSession = gson.fromJson(testJSON, Session.class);
         testLayoutedSnapshot.setPane(testPane);
 
-        Mockito.doReturn(testSession).when(testParser).parse(any());
-        Mockito.doReturn(testParser).when(testModule).getParser();
-        Mockito.doReturn(testLayouter).when(testModule).getLayouter();
-        Mockito.doReturn(testLayoutedSnapshot).when(testLayouter).layout(any());
-
         Map<String, ADVModule> modules = new HashMap<>();
-        modules.put("testModule", testModule);
+        modules.put("test", testModule);
         testModuleStore.setAvailableModules(modules);
 
+        Mockito.doReturn(testParser).when(testModule).getParser();
+        Mockito.doReturn(testLayouter).when(testModule).getLayouter();
+
+        Mockito.doReturn(testSession).when(testParser).parse(any());
+        Mockito.doReturn(testLayoutedSnapshot).when(testLayouter).layout(any());
     }
 
     @Test
     public void processTest() {
-        String testJSON = "{\"moduleName\": \"testModule\"}";
         flowControl.process(testJSON);
         List<Session> sessions = testSessionStore.getSessions();
         assertTrue(sessions.contains(testSession));
 
         long testSessionId = testSession.getSessionId();
-        List<Pane> panes = testLayoutedSnapshotStore.getSnapshotPanes(testSessionId);
+        List<Pane> panes = testLayoutedSnapshotStore
+                .getSnapshotPanes(testSessionId);
         assertTrue(panes.contains(testPane));
-    }
-
-    public static class Module extends JukitoModule {
-
-        @Override
-        protected void configureTest() {
-            forceMock(Session.class);
-        }
+        assertTrue(testLayoutedSnapshotStore
+                .hasSnapshot(testSessionId, testLayoutedSnapshot
+                        .getSnapshotId()));
     }
 
 
+    @Test
+    public void processDuplicatedSessionTest() {
+        flowControl.process(testJSON);
+        flowControl.process(testJSON);
+        assertEquals(1, testSessionStore.getSessions().size());
+        assertEquals(1, testLayoutedSnapshotStore.getLayoutedSnapshots(testSession.getSessionId()).size());
+    }
 }
