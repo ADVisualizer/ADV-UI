31a72fb743ba2297c456e01e01f562d326692d2c
Murièle Trentini <muriele@trentini.ch>
2018-05-09 10:09:05 +0200

    make a more complex layout: use a grid instead of adding all panes horizontally (just a suggestion)

--- ui-core/src/main/java/ch/hsr/adv/ui/core/logic/CoreLayouter.java
+++ ui-core/src/main/java/ch/hsr/adv/ui/core/logic/CoreLayouter.java
@@ -6,7 +6,9 @@ import javafx.scene.control.SplitPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * The Core Layouter wraps the panes of the module layouter in a SplitPane.
@@ -21,9 +23,38 @@ public class CoreLayouter {
      * @return split pane
      */
     public Region layout(List<Pane> panes) {
+        Queue<Pane> remainingPanes = new LinkedList<>(panes);
         SplitPane parentPane = new SplitPane();
-        parentPane.setOrientation(Orientation.HORIZONTAL);
-        parentPane.getItems().addAll(panes);
+        parentPane.setOrientation(Orientation.VERTICAL);
+
+        // calculate dimensions of the grid
+        int columns = 1;
+        int rows = 1;
+        while (columns * rows < remainingPanes.size()) {
+            columns++;
+            if (columns * rows < remainingPanes.size()) {
+                rows++;
+            }
+        }
+
+        // fill the grid
+        SplitPane rowPane;
+        for (int rowCount = 0; rowCount < rows; rowCount++) {
+            rowPane = addNewRow(parentPane);
+            for (int colCount = 0; colCount < columns; colCount++) {
+                if (!remainingPanes.isEmpty()) {
+                    rowPane.getItems().add(remainingPanes.poll());
+                }
+            }
+        }
+
         return parentPane;
+    }
+
+    private SplitPane addNewRow(SplitPane parentPane) {
+        SplitPane rowPane = new SplitPane();
+        rowPane.setOrientation(Orientation.HORIZONTAL);
+        parentPane.getItems().add(rowPane);
+        return rowPane;
     }
 }
