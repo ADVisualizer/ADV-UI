f7a9153d88b97d622a40cb42aec314abfc7642a1
Michael Wieland <michi.wieland@hotmail.com>
2018-05-14 14:00:46 +0200

    unified default style and default line style

--- ui-core/src/main/java/ch/hsr/adv/ui/core/logic/domain/styles/presets/ADVDefaultLineStyle.java
+++ ui-core/src/main/java/ch/hsr/adv/ui/core/logic/domain/styles/presets/ADVDefaultLineStyle.java
@@ -1,33 +1,23 @@
 package ch.hsr.adv.ui.core.logic.domain.styles.presets;
 
 import ch.hsr.adv.ui.core.logic.domain.styles.ADVStrokeStyle;
-import ch.hsr.adv.ui.core.logic.domain.styles.ADVStyle;
 
 /**
  * The default implementation of the ADV Style sets standard values for all
  * style variables.
  */
-public class ADVDefaultLineStyle implements ADVStyle {
-    private static final int BLACK = 0;
-    private static final int DEFAULT_THICKNESS = 1;
+public class ADVDefaultLineStyle extends ADVDefaultStyle {
 
-    @Override
-    public int getFillColor() {
-        return BLACK;
-    }
-
-    @Override
-    public int getStrokeColor() {
-        return BLACK;
-    }
+    private static final int STROKE_THICKNESS = 1;
+    private static final String STOKE_STYLE = ADVStrokeStyle.SOLID.getStyle();
 
     @Override
     public String getStrokeStyle() {
-        return ADVStrokeStyle.SOLID.getStyle();
+        return STOKE_STYLE;
     }
 
     @Override
     public int getStrokeThickness() {
-        return DEFAULT_THICKNESS;
+        return STROKE_THICKNESS;
     }
 }
