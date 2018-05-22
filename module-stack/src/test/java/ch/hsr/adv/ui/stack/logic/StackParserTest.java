4789d23a4a901c463ff72c64210d9efff672e367
Murièle Trentini <muriele@trentini.ch>
2018-05-22 10:32:32 +0200

    remove redundant stringifyer add missing stackParser test

---
+++ module-stack/src/test/java/ch/hsr/adv/ui/stack/logic/StackParserTest.java
@@ -0,0 +1,61 @@
+package ch.hsr.adv.ui.stack.logic;
+
+import ch.hsr.adv.commons.array.logic.domain.ArrayElement;
+import ch.hsr.adv.commons.core.logic.domain.ADVElement;
+import ch.hsr.adv.commons.core.logic.domain.ModuleGroup;
+import ch.hsr.adv.commons.stack.logic.domain.StackElement;
+import ch.hsr.adv.ui.core.access.FileDatastoreAccess;
+import ch.hsr.adv.ui.core.logic.exceptions.ADVParseException;
+import ch.hsr.adv.ui.stack.logic.StackParser;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.inject.Inject;
+import org.jukito.JukitoRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JukitoRunner.class)
+public class StackParserTest {
+
+    private JsonElement jsonElement;
+
+    @Inject
+    private StackParser sut;
+
+    @Before
+    public void setup(FileDatastoreAccess reader) throws IOException {
+        URL url = getClass().getClassLoader().getResource("module-group.json");
+        String json = reader.read(new File(url.getPath()));
+
+        Gson gson = new Gson();
+        jsonElement = gson.fromJson(json, JsonElement.class);
+    }
+
+    @Test
+    public void parseSessionDetailsTest() throws ADVParseException {
+        // WHEN
+        ModuleGroup actual = sut.parse(jsonElement);
+
+        // THEN
+        assertEquals("stack", actual.getModuleName());
+    }
+
+    @Test
+    public void parseADVElementToStackElementTest() throws ADVParseException {
+        // WHEN
+        ModuleGroup actual = sut.parse(jsonElement);
+        ADVElement element = actual.getElements().get(0);
+
+        // THEN
+        assertEquals(StackElement.class, element.getClass());
+        StackElement stackElement = (StackElement) element;
+        assertEquals("1", stackElement.getContent());
+    }
+}
